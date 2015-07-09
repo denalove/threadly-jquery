8b314b87429d25f8a3364194d97a0c63f619cd8d
Dena Sawyer <denasawyer1111@gmail.com>
2015-07-09 15:47:18 -0400

    Add functionality to jquert to add comments

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+var template = function(comment) {
+  return '<ul class="comments"><li>' + comment + '</li></ul>';
+  };
+
 var main = function() {
   $('form').submit(function() {
-    
+    var comment = $('#comment').val();
+   	
+
     if(comment !== "") {
-      var html = $('<li>').text(comment);
+      var html = template(comment);
+      $(html).prependTo('.comments');
+      $('#comment').val("");
       
     }
 
